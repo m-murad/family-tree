163affb35f14a40fcf2a697a2d3436f578e2a202
m-murad <murad.kuka@gmail.com>
2020-01-14 17:03:03 +0530

    initial commit
    Signed-off-by: m-murad <murad.kuka@gmail.com>

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "family-tree",
+	Short: "Define a query a family",
+	Long: `This program lets you to create a family. 
+You can add members to the family and define relationships between members.`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/m-murad/finleap/cmd"
+
+func main() {
+	cmd.Execute()
+}
